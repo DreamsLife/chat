fe7f229eec83416649cb906c2b7c22dbb948be8d
DreamsLife <36564646+DreamsLife@users.noreply.github.com>
2025-07-24 22:08:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ wss.on("connection", ws => {
 
 app.get("/", (req, res) => res.send("WebSocket chat server up!"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
